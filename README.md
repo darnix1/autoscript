d315303771346556f170083e685e34b884e96736
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-03-16 19:52:59 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# autoscript
+# UP REPO DEBIAN
+<pre><code>apt update -y && apt upgrade -y && update-grub</code></pre>
+  
+  
+# UP REPO UBUNTU
+<pre><code>apt update -y && apt upgrade -y && apt dist-upgrade -y</code></pre>
+ 
+
+### INSTALL SCRIPT 
+<pre><code>apt install -y && wget -q https://raw.githubusercontent.com/ALAWI-VPN/autoscript/main/setup.sh && chmod +x setup.sh && screen -S alawi bash setup.sh</code></pre>
+
+### WORK DI OS
+- UBUNTU 18 / 20
+- DEBIAN 9  / 10 ( recommended )
+### SUPPORT PORT
+```
+- SSL/TLS : 443 / 8443
+- SSHWS   : 80 / 8880 / 8080 / 2082 / 2095 / 2096 / 2022 / 2092
+```
---
+++ update.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+clear
+# color fun
+grs="\033[1;93m"
+bg="\033[42m"
+gr="\033[1;32m"
+gry="\033[1;37m"
+NC='\033[0m'
+
+# function bar animasi
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne " ${grs}Sedang memasang ${gry}- ${grs}["
+    while true; do
+        for ((i = 0; i < 11; i++)); do
+            echo -ne "${gr}➣"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "${grs}]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne " ${grs}Sedang memasang ${gry}- ${grs}["
+    done
+    echo -e "${grs}]${gry} - ${gr}Selesai !${gry}"
+    tput cnorm
+}
+res1() {
+    wget https://raw.githubusercontent.com/ALAWI-VPN/autoscript/main/menu/menu.zip
+    unzip menu.zip
+    chmod +x menu/*
+    mv menu/* /usr/local/sbin/
+    #unzip menu.zip
+    #chmod +x menu/*
+    #mv menu/* /usr/local/sbin
+    rm -rf menu menu.zip
+    rm -rf update.sh
+    wget -qO- fv-tunnel "https://raw.githubusercontent.com/ALAWI-VPN/autoscript/main/config/fv-tunnel" 
+    chmod +x fv-tunnel 
+    bash fv-tunnel
+    rm -rf fv-tunnel
+}
+netfilter-persistent
+clear
+echo -e "${grs}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${bg}              UPDATE SCRIPT               ${NC}"
+echo -e "${grs}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+echo -e " ${gr}UPDATING SCRIPT PREMIUM${NC}"
+fun_bar 'res1'
+echo -e "${grs}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+sleep 3
+menu
+
